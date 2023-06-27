ffd73a8ae57ab45ddb98fb875242a2edc5801375
uzakotim <timauzakov@gmail.com>
2023-06-27 15:43:11 +0500

    views from handlebars

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 var express = require('express');
 var app = express();
 
+// set up handlebars vew engine
+var handlebars = require('express3-handlebars').create({ defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res){
-   res.type('text/plain');
-   res.send('Clothings!');
+   res.render('home');
 });
 app.get('/about', function(req, res){
-   res.type('text/plain');
-   res.send('About Clothings!');
+   res.render('about');
 });
 //custom 404 page
 app.use(function(req,res,next){
-   res.type('text/plain');
    res.status(404);
-   res.send('404 - Not Found');
+   res.render('404');
 });
 
 app.use(function(err,req,res,next){
    console.error(err.stack);
-   res.type('text/plain');
    res.status(500);
-   res.send('500 - Server Error');
+   res.render('500');
 });
 
 app.listen(app.get('port'), function(){
