4a9a5b311db586e256f35115e566329162ce50c6
uzakotim <timauzakov@gmail.com>
2023-06-27 15:06:16 +0500

    example app

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-const express = require("express");
-const app = express();
-const passport = require("passport");
-require("./config/passport")(passport);
-const flash = require("connect-flash");
+var express = require('express');
+var app = express();
 
-app.use(flash());
-
-app.set("view engine", "ejs");
-app.set("views", __dirname + "/views");
-
-app.get("/", function (req, res) {
-    res.render("index", { message: req.flash("loginMessage") });
+app.get('/', function (req, res) {
+   res.send('hello,world!');
 });
 
-app.listen(3000, () => {
-    console.log("Example app listening on port 3000!");
+app.listen(3000, function(){
+   console.log('Example app listening on port 3000!');
 });
-app.post(
-    "/login",
-    passport.authenticate("local", {
-        successRedirect: "/",
-        failureRedirect: "/login",
-    })
-);
