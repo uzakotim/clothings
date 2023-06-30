4ed575fb3743a97f9df664d92c7f1d7329abcb52
uzakotim <timauzakov@gmail.com>
2023-06-30 08:13:56 +0500

    adding partials

--- app.js
+++ app.js
@@ -5,7 +5,29 @@ var app = express();
 // set up handlebars vew engine
 var handlebars = require("express3-handlebars").create({
     defaultLayout: "main",
+    helpers: {
+        section: function (name, options) {
+            if (!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        },
+    },
 });
+
+function getWeatherData() {
+    return {
+        locations: [
+            {
+                name: "Portland",
+                forecastUrl:
+                    "http://www.wundeground.com/US/OR/Portland.html",
+                iconUrl: "http://icons-ak.wxug.com/i/c/k/cloudy.gif",
+                weather: "Overcast",
+                temp: "54.1 F (12.3 C)",
+            },
+        ],
+    };
+}
 
 app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
@@ -17,10 +39,23 @@ app.use(function (req, res, next) {
         app.get("env") !== "production" && req.query.test == "1";
     next();
 });
+app.use(function (req, res, next) {
+    if (!res.locals.partials) res.locals.partials = {};
+    res.locals.partials.weather = getWeatherData();
+    next();
+});
+app.disable("x-powered-by");
 
 //routes go here...
 app.get("/", function (req, res) {
     res.render("home");
+});
+app.get("/headers", function (req, res) {
+    res.set("Content-Type", "text/plain");
+    var s = "";
+    for (var name in req.headers)
+        s += name + ": " + req.headers[name] + "\n";
+    res.send(s);
 });
 app.get("/about", function (req, res) {
     res.render("about", {
@@ -38,16 +73,16 @@ app.get("/shops/waikiki", function (req, res) {
 app.get("/shops/request-cloth", function (req, res) {
     res.render("shops/request-cloth");
 });
-//custom 404 page
-app.use(function (req, res, next) {
-    res.status(404);
-    res.render("404");
-});
 
 app.use(function (err, req, res, next) {
     console.error(err.stack);
     res.status(500);
     res.render("500");
+});
+//custom 404 page
+app.use(function (req, res, next) {
+    res.status(404);
+    res.render("404");
 });
 
 app.listen(app.get("port"), function () {
