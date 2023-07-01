2d4a4dac14f24bf21c1bd18bd99edcfe65ad7137
uzakotim <timauzakov@gmail.com>
2023-07-01 09:22:05 +0500

    file upload

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-var fortune = require("./lib/fortune.js");
-var express = require("express");
+var express = require("express"),
+    fortune = require("./lib/fortune.js"),
+    formidable = require("formidable"),
+    jqupload = require("jquery-file-upload-middleware");
+
 var app = express();
 
-// set up handlebars vew engine
+// set up handlebars view engine
 var handlebars = require("express3-handlebars").create({
     defaultLayout: "main",
     helpers: {
@@ -13,52 +16,93 @@ var handlebars = require("express3-handlebars").create({
         },
     },
 });
+app.engine("handlebars", handlebars.engine);
+app.set("view engine", "handlebars");
 
+app.set("port", process.env.PORT || 3000);
+
+app.use(express.static(__dirname + "/public"));
+
+app.use(require("body-parser")());
+
+// set 'showTests' context property if the querystring contains test=1
+app.use(function (req, res, next) {
+    res.locals.showTests =
+        app.get("env") !== "production" && req.query.test === "1";
+    next();
+});
+
+// mocked weather data
 function getWeatherData() {
     return {
         locations: [
             {
                 name: "Portland",
                 forecastUrl:
-                    "http://www.wundeground.com/US/OR/Portland.html",
+                    "http://www.wunderground.com/US/OR/Portland.html",
                 iconUrl: "http://icons-ak.wxug.com/i/c/k/cloudy.gif",
                 weather: "Overcast",
                 temp: "54.1 F (12.3 C)",
+            },
+            {
+                name: "Bend",
+                forecastUrl: "http://www.wunderground.com/US/OR/Bend.html",
+                iconUrl: "http://icons-ak.wxug.com/i/c/k/partlycloudy.gif",
+                weather: "Partly Cloudy",
+                temp: "55.0 F (12.8 C)",
+            },
+            {
+                name: "Manzanita",
+                forecastUrl:
+                    "http://www.wunderground.com/US/OR/Manzanita.html",
+                iconUrl: "http://icons-ak.wxug.com/i/c/k/rain.gif",
+                weather: "Light Rain",
+                temp: "55.0 F (12.8 C)",
             },
         ],
     };
 }
 
-app.engine("handlebars", handlebars.engine);
-app.set("view engine", "handlebars");
-app.set("port", process.env.PORT || 3000);
-
-app.use(express.static(__dirname + "/public"));
-app.use(function (req, res, next) {
-    res.locals.showTests =
-        app.get("env") !== "production" && req.query.test == "1";
-    next();
-});
+// middleware to add weather data to context
 app.use(function (req, res, next) {
     if (!res.locals.partials) res.locals.partials = {};
-    res.locals.partials.weather = getWeatherData();
+    res.locals.partials.weatherContext = getWeatherData();
     next();
 });
-app.disable("x-powered-by");
 
-//routes go here...
+// jQuery File Upload endpoint middleware
+app.use("/upload", function (req, res, next) {
+    var now = Date.now();
+    jqupload.fileHandler({
+        uploadDir: function () {
+            return __dirname + "/public/uploads/" + now;
+        },
+        uploadUrl: function () {
+            return "/uploads/" + now;
+        },
+    })(req, res, next);
+});
+
 app.get("/", function (req, res) {
     res.render("home");
 });
+app.get("/about", function (req, res) {
+    res.render("about", {
+        fortune: fortune.getFortune(),
+        pageTestScript: "/qa/tests-about.js",
+    });
+});
+app.get("/shops/reserved", function (req, res) {
+    res.render("shops/reserved");
+});
+app.get("/shops/waikiki", function (req, res) {
+    res.render("shops/waikiki");
+});
+app.get("/shops/request-cloth", function (req, res) {
+    res.render("shops/request-cloth");
+});
 app.get("/jquery-test", function (req, res) {
     res.render("jquery-test");
-});
-app.get("/headers", function (req, res) {
-    res.set("Content-Type", "text/plain");
-    var s = "";
-    for (var name in req.headers)
-        s += name + ": " + req.headers[name] + "\n";
-    res.send(s);
 });
 app.get("/nursery-rhyme", function (req, res) {
     res.render("nursery-rhyme");
@@ -71,32 +115,54 @@ app.get("/data/nursery-rhyme", function (req, res) {
         noun: "heck",
     });
 });
-app.get("/about", function (req, res) {
-    res.render("about", {
-        fortune: fortune.getFortune(),
-        pageTestScript: "/qa/tests-about.js",
+app.get("/thank-you", function (req, res) {
+    res.render("thank-you");
+});
+app.get("/newsletter", function (req, res) {
+    // we will learn about CSRF later...for now, we just
+    // provide a dummy value
+    res.render("newsletter", { csrf: "CSRF token goes here" });
+});
+
+app.post("/process", function (req, res) {
+    if (req.xhr || req.accepts("json,html") === "json") {
+        // if there were an error, we would send { error: 'error description' }
+        res.send({ success: true });
+    } else {
+        // if there were an error, we would redirect to an error page
+        res.redirect(303, "/thank-you");
+    }
+});
+app.get("/contest/cloth-photo", function (req, res) {
+    var now = new Date();
+    res.render("contest/cloth-photo", {
+        year: now.getFullYear(),
+        month: now.getMonth(),
+    });
+});
+app.post("/contest/cloth-photo/:year/:month", function (req, res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        if (err) return res.redirect(303, "/error");
+        console.log("received fields:");
+        console.log(fields);
+        console.log("received files:");
+        console.log(files);
+        res.redirect(303, "/thank-you");
     });
 });
 
-app.get("/shops/reserved", function (req, res) {
-    res.render("shops/reserved");
-});
-app.get("/shops/waikiki", function (req, res) {
-    res.render("shops/waikiki");
-});
-app.get("/shops/request-cloth", function (req, res) {
-    res.render("shops/request-cloth");
+// 404 catch-all handler (middleware)
+app.use(function (req, res, next) {
+    res.status(404);
+    res.render("404");
 });
 
+// 500 error handler (middleware)
 app.use(function (err, req, res, next) {
     console.error(err.stack);
     res.status(500);
     res.render("500");
-});
-//custom 404 page
-app.use(function (req, res, next) {
-    res.status(404);
-    res.render("404");
 });
 
 app.listen(app.get("port"), function () {
